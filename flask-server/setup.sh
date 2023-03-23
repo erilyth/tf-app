12961ff52aa441c005d0abff0781c5d23d049d7a
Vishal Batchu <vishalbatchu@google.com>
2023-03-23 05:47:48 +0000

    Remove additional setup.sh that was not required

--- flask-server/setup.sh
+++
@@ -1,8 +0,0 @@
-# Install and start docker
-
-# Install flask and other server deps
-pip install -r requirements.txt
-
-# Install react
-# sudo apt install npm
-# sudo npm -g install create-react-app

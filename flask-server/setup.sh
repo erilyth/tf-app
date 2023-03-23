1ccdb07b25e38e0b361a316780016df3d89c95df
Vishal Batchu <vishalbatchu@google.com>
2023-03-23 05:16:23 +0000

    Hello world flask app with a Dockerfile to create a docker image for the app

---
+++ flask-server/setup.sh
@@ -0,0 +1,8 @@
+# Install and start docker
+
+# Install flask and other server deps
+pip install -r requirements.txt
+
+# Install react
+# sudo apt install npm
+# sudo npm -g install create-react-app

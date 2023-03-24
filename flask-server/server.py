ed968cfa74d3410731fcdfe7d06cdcf231934232
erilyth <vishalvenkat71@gmail.com>
2023-03-24 14:52:27 +0000

    Test pre-push hooks

--- flask-server/server.py
+++ flask-server/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Route the index page
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return 'Hello world test!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)

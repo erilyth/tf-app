12b2fe44377716d532a0a3f7929d97572d62a27c
erilyth <vishalvenkat71@gmail.com>
2023-03-24 15:48:54 +0000

    Update automation scripts to work from any dir

--- automation/README.md
+++ automation/README.md
@@ -1,4 +1,3 @@
 #### Notes
 
-- Always run the automation scripts from within the automation directory.
 - The service account keys required should be downloaded from GCP and stored as "../service_acc_keys/service-acc-key.json"
--- automation/deploy_kubernetes.sh
+++ automation/deploy_kubernetes.sh
@@ -1,6 +1,6 @@
 # Delete and recreate the key in case the service account has been updated.
 kubectl delete secret gcr-key --ignore-not-found
-kubectl create secret docker-registry gcr-key --docker-server=gcr.io --docker-username=_json_key --docker-password="$(cat ../service_acc_keys/service-acc-key.json)"
+kubectl create secret docker-registry gcr-key --docker-server=gcr.io --docker-username=_json_key --docker-password="$(cat $(git rev-parse --show-toplevel)/service_acc_keys/service-acc-key.json)"
 
 kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "gcr-key"}]}'
 
@@ -8,13 +8,13 @@ kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "gcr-key
 if [ "$(kubectl get deployments | grep -c 'flask-server-deployment')" -ge 1 ]; then
     kubectl rollout restart deployment/flask-server-deployment
 else
-    kubectl create -f ../kubernetes/flask-server.yaml
+    kubectl create -f $(git rev-parse --show-toplevel)/kubernetes/flask-server.yaml
 fi
 
 kubectl get pods
 kubectl get services
 kubectl describe service flask-server-service
 
-# Run port forwarding to access the flask-server-service through a browser since our 
+# Manually run port forwarding to access the flask-server-service through a browser since our 
 # service uses an internal only load balancer.
-kubectl port-forward service/flask-server-service 8080:8080
+# kubectl port-forward service/flask-server-service 8080:8080
